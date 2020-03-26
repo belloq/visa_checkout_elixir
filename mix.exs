d3a451c54423abad203909a12d795773cfcc6ab3
belloq <unlimitedstress@gmail.com>
2020-03-26 16:20:32 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VisaCheckoutElixir.Mixfile do
   def project do
     [
       app: :visa_checkout_elixir,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
